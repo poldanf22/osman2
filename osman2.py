33e736f286d6dcfda868cb2efa254fddf5cef6d4
poldanf22 <108033529+poldanf22@users.noreply.github.com>
2023-11-21 07:51:24 +0700

    PENILAIAN SAS

--- osman2.py
+++ osman2.py
@@ -20960,7 +20960,7 @@ if authentication_status:
         with col8:
             PENILAIAN = st.selectbox(
                 "PENILAIAN",
-                ("--Pilih Penilaian--", "SUMATIF TENGAH SEMESTER"))
+                ("--Pilih Penilaian--", "SUMATIF TENGAH SEMESTER", "SUMATIF AKHIR SEMESTER"))
 
         col9 = st.container()
 
